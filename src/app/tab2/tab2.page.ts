94cab602aad0f3dd38a243bfc0635574af3f0bbe
bwebstar <bryan@thewebstar.net>
2023-04-13 10:51:31 -0400

    Enabling Photo Capture

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { UserPhoto, PhotoService } from '../services/photo.service';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, CommonModule],
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
 
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+         }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
